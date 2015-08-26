5fe09a8b8180182dd3221d49921769cbe84d6cbc
Victor Galitsky <concept.galitsky@gmail.com>
2015-08-26 11:36:54 +0300

    activity feed

--- app/mod/manage/model/activity/collection.php
+++ app/mod/manage/model/activity/collection.php
@@ -2,7 +2,7 @@
 class manage_model_activity_collection extends core_collection{
 
     public function activityReferenceForMobile(){
-        $sql = "SELECT name FROM {$this->getTable()}";
+        $sql = "SELECT activity_id , name FROM {$this->getTable()}";
         $this->setSql($sql);
         return $this;
     }
